fc18677c2c9c5d270f6f37e5f18d3442dce6c254
Nathaniel W <52637288+nw-github@users.noreply.github.com>
2024-05-06 20:59:31 -0400

    replace frontend with typescript+swc frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
