a169c86c8d56b9e8345c01bd232106d55639930e
izzy <ismaelnrqz@gmail.com>
2024-05-10 13:48:55 -0400

    brought the changes back

--- server/src/seeds/init.ts
+++ server/src/seeds/init.ts
@@ -1,62 +1,49 @@
+import { Event } from "../models/Event";
 import Post from "../models/Post";
+import Reservation from "../models/Reservation";
 import User from "../models/User";
-import { Knex } from "knex";
 
-export const seed = async (knex: Knex) => {
-  // Before you have models you can always just do `await knex('table_name').del`
-  await knex("users").del();
-  await knex("posts").del();
-  await knex("images").del();
-  await knex("reservation").del();
-  await knex("event_log").del();
+export const seed = async () => {
+  await Event.deleteAll();
+  await Reservation.deleteAll();
+  await Post.deleteAll();
+  await User.deleteAll();
 
-  await knex.raw("ALTER SEQUENCE users_id_seq RESTART WITH 1");
-  await knex.raw("ALTER SEQUENCE posts_id_seq RESTART WITH 1");
-  await knex.raw("ALTER SEQUENCE images_id_seq RESTART WITH 1");
-  await knex.raw("ALTER SEQUENCE reservation_id_seq RESTART WITH 1");
-  await knex.raw("ALTER SEQUENCE event_log_id_seq RESTART WITH 1");
-
-  /// Insert fake users
   const cool_cat = (await User.create("cool_cat", "1234"))!;
-  await User.create("l33t-guy", "1234");
-  await User.create("wowow", "1234");
- 
-  await Post.create(
-    cool_cat,
+  const leet_guy = (await User.create("l33t-guy", "1234"))!;
+  const wowow = (await User.create("wowow", "1234"))!;
+
+  const first = await Post.create(
+    cool_cat.data.id,
     "First Post",
     "This is my first post. Here is a PS5 if anyone needs it",
     "Brooklyn",
     ["https://cdn.mos.cms.futurecdn.net/HkdMToxijoHfz4JwUgfh3G.jpg"],
+    [new Date()]
   );
 
   await Post.create(
-    cool_cat,
+    cool_cat.data.id,
     "Second Post",
     "I am a software developer. I need to sell this laptop",
     "Brooklyn",
-    ["https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg"]
+    ["https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg"],
+    [new Date()]
   );
 
   await Post.create(
-    cool_cat,
+    cool_cat.data.id,
     "Third Post",
     "How do you knw youre not in the matrix!?!? #StayWoke. Also here a gift if anyone wants it",
     "Brooklyn",
     [
       "https://cdn.thewirecutter.com/wp-content/media/2023/05/sofabuyingguide-2048px-benchmademoderncream.jpg?auto=webp&quality=75&width=1024",
-    ]
+    ],
+    [new Date()]
   );
 
-  // Insert fake reservations
-  await knex("reservation").insert([
-    { pickup_time: new Date(), user_id: 1, post_id: 1 },
-    { pickup_time: new Date(), user_id: 2, post_id: 2 },
-  ]);
-
-  // Insert fake event logs
-  await knex("event_log").insert([
-    { event: "reserved", actor_id: 1, post_id: 1 },
-    { event: "created", actor_id: 2, post_id: 2 },
-  ]);
+  let [rev] = await Reservation.byPost(first.data.id);
+  rev = (await rev.select(leet_guy.data.id))!;
+  rev = (await rev.cancel(leet_guy.data.id))!;
+  rev = (await rev.select(wowow.data.id))!;
 };
-
