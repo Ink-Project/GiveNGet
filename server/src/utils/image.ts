26ad3ef1bfeb84ef8d0d7d322a80531081ea9321
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2024-05-16 12:00:31 -0400

    remove code to manually handle base64 decoding
    fetch is already capable of handling base64 urls with a MIME type

--- server/src/utils/image.ts
+++ server/src/utils/image.ts
@@ -9,36 +9,11 @@ const exts: Partial<Record<string, string>> = {
   "image/png": "png",
   "image/webp": "webp",
   "image/jpeg": "jpeg",
+  "image/bmp": "bmp",
 };
 
-/** Try to get the content type by the image magic header */
-const getContentType = (buf: Buffer) => {
-  const u1 = buf.readUint32BE(0);
-  const u2 = buf.readUint32BE(4);
-  const u3 = buf.readUint32BE(8);
-  if (u1 === 0x89504e47 && u2 === 0x0d0a1a0a) {
-    return "image/png";
-  } else if (u1 === 0xffd8ffe0 || u1 === 0xffd8ffe1 || u1 === 0xffd8ffee) {
-    return "image/jpeg";
-  } else if (u1 === 0x52494646 && u2 === buf.length && u3 === 0x57454250) {
-    return "image/webp";
-  }
-};
-
-/**
- * Get the image data from `image`. If it's base64 encoded image data, decode it. If it's a URL,
- * download the image.
- */
+/** Get the image data from `image`. */
 const getImageData = async (image: string) => {
-  if (image.startsWith("base64:")) {
-    const data = Buffer.from(image.slice(7), "base64");
-    const typ = getContentType(data);
-    if (!typ) {
-      throw new Error("Unsupported base64 encoded image type (must be png, webp, or jpg)");
-    }
-    return { data, ext: exts[typ] };
-  }
-
   const resp = await fetch(image);
   const ct = resp.headers.get("Content-Type");
   const ext = exts[ct?.split(";")?.[0] || ""];
@@ -47,7 +22,7 @@ const getImageData = async (image: string) => {
       `'${image}' returned unsupported content type ${ct} (must be png, webp, or jpg)`
     );
   }
-  return { data: new DataView(await resp.blob().then((buf) => buf.arrayBuffer())), ext };
+  return { data: new Uint8Array(await resp.arrayBuffer()), ext };
 };
 
 /**
